fdeaf8f059abdfe615afaaa23d3e8fb3dc8f602b
aniki <aniki@MacBook-Air-de-Brahim.local>
2021-12-16 15:37:19 +0100

    Creation du composant exercice et ajout dans app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { Exercice2Component } from './exercice2/exercice2.component';
+import { AppComponent } from "./app.component";
+import { Exercice2Component } from "./exercice2/exercice2.component";
+import { Exercice1Component } from "./exercice1/exercice1.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Exercice2Component
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Exercice2Component, Exercice1Component],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
