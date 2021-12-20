95961030aaf339e6a6c47b6c2667cb489f907398
aniki <aniki@MacBook-Air-de-Brahim.local>
2021-12-20 10:03:30 +0100

    exercice 4 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { Exercice2Component } from "./exercice2/exercice2.component";
 import { Exercice1Component } from "./exercice1/exercice1.component";
+import { Exxercice3Component } from './exxercice3/exxercice3.component';
+import { Exercice4Component } from './exercice4/exercice4.component';
 
 @NgModule({
-  declarations: [AppComponent, Exercice2Component, Exercice1Component],
+  declarations: [AppComponent, Exercice2Component, Exercice1Component, Exxercice3Component, Exercice4Component],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
