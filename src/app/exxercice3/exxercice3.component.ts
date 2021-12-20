31b31f97107b0c61933ebfaa0dbad694e12e7ef7
aniki <aniki@MacBook-Air-de-Brahim.local>
2021-12-20 09:52:02 +0100

    exercice 3 done

---
+++ src/app/exxercice3/exxercice3.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-exxercice3",
+  templateUrl: "./exxercice3.component.html",
+  styleUrls: ["./exxercice3.component.css"],
+})
+export class Exxercice3Component implements OnInit {
+  public statut: boolean = true;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
